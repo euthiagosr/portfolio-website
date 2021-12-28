f81d334f4d919823beab5fbf2a7a9dc0a1cb4842
Thiago <thiago.a.rodrigues@accenture.com>
2021-12-28 21:21:33 +0000

    Portfolio adjustments

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -81,7 +81,7 @@ export default function Portfolio() {
 
             <div className="container">
                 {data.map((d) => (
-                    <div className="item">
+                <div className="item">
                     <img src={d.img} alt="" />
                     <h3>{d.title}</h3>
                 </div>
